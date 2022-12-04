16f77ff6fe08ed6c4850b0cd4cfbd123d3f20bf7
Yani <devyani.maladkar@gmail.com>
2022-12-04 12:10:24 -0600

    code rearrange

--- GNNModels/generate_explanations.py
+++
@@ -1,7 +0,0 @@
-from Explainers import *
-
-createExplanations(model_name='GCN',dataset_name='Cora',type='NC')
-createExplanations(model_name='GCN',dataset_name='CiteSeer',type='NC')
-
-createExplanations(model_name='GAT',dataset_name='Cora',type='NC')
-createExplanations(model_name='GAT',dataset_name='CiteSeer',type='NC')
